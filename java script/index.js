f5c4e1342cd72e800897b3db8b7cbc7a81b17cf9
Tarun-Raj01 <tarunraj7204@gmail.com>
2026-02-25 15:26:28 +0530

    change commit

---
+++ java script/index.js
@@ -0,0 +1,22 @@
+// console.log("hi heloooo")
+// document.writeln("hi macgiiii")
+
+// let str1="tarun"
+// console.log(str1,typeof str1);
+
+
+// let fun1=function street(){
+//     console.log("hiii");
+//     return "heooo";
+// }
+// console.log(street());
+let fun1=()=>{
+    console.log("hiiii tarunnnananan")
+}
+fun1();
+let fun2=(a,b)=>a*b;
+console.log(fun2(2,2));
+let fun3=a=>a*a;
+console.log(fun3(5))
+
+
---
+++ java/logics.java
@@ -0,0 +1,10 @@
+class sample{
+    public static String main(String[] args) {
+        String s="Hello World";
+        String g=s.substring(s.length()-2);
+        
+
+        return g+""+g+""+g;
+
+    }
+    }
